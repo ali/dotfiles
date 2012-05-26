784afd3cfd096782666f24d4f83255e429d84d69
Ali Ukani <ali.ukani@gmail.com>
2012-05-26 03:53:30 -0400

    Adding in my emacs config

---
+++ emacs.d/init.el
@@ -0,0 +1,10 @@
+;; emacs configuration
+
+;; Add column numbers to the mode line
+(column-number-mode t)
+
+;; Hilight parenthesis
+(show-paren-mode t)
+
+;; Convenient way to open files and switch buffers
+(ido-mode t)
